d92d27ed06d4ddff26084cc898ece5db497e64f4
DAM6060 <ddimitar98@gmail.com>
2024-10-30 14:31:50 +0200

    Moved Scripts to Different folder so that they can work
    Need to figure out why they are not being picked up

---
+++ Assets/TutorialInfo/Scripts/PlayerControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace Assets.TutorialInfo.Scripts
+{
+	public class PlayerControllerTest: MonoBehaviour
+	{
+		private void Start()
+		{
+			
+		}
+		private void Update()
+		{
+		
+		}
+	}
+}
