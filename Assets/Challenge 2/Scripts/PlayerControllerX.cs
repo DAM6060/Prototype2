999c32f53a2a61966af5b5eb9f7900e9479bcd0e
DAM6060 <ddimitar98@gmail.com>
2024-11-01 18:21:29 +0200

    Challenge 2 Complete
    Given a game with glitches to be fixed

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+	public GameObject dogPrefab;
+	public float timer = 0.0f;
+	public float allowedSpawnRate = 2.0f;
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		timer += Time.deltaTime;
+		
+		if (timer > allowedSpawnRate && Input.GetKeyDown(KeyCode.Space))
+		{
+			Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+			timer = 0.0f;
+		}
+
+	}
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 0.0f;
+
+    private int rangeMin = 3;
+	private int rangeMax = 5;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		// Start the coroutine to spawn balls
+		StartCoroutine(SpawnRandomBallCoroutine());
+	}
+
+	// Coroutine to spawn random balls at random intervals
+	private IEnumerator SpawnRandomBallCoroutine()
+	{
+		yield return new WaitForSeconds(startDelay); // Wait for the start delay
+
+		while (true) // Loop indefinitely
+		{
+			SpawnRandomBall(); // Call the method to spawn a ball
+
+			// Generate a random interval between rangeMin and rangeMax
+			float randomInterval = Random.Range(rangeMin, rangeMax);
+			yield return new WaitForSeconds(randomInterval); // Wait for the random interval
+		}
+	}
+
+	// Spawn random ball at random x position at top of play area
+	void SpawnRandomBall()
+	{
+		// Generate random spawn position
+		Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+		// Instantiate a random ball prefab at the spawn position
+		int randomIndex = Random.Range(0, ballPrefabs.Length);
+		Instantiate(ballPrefabs[randomIndex], spawnPos, ballPrefabs[randomIndex].transform.rotation);
+	}
+}
